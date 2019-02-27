82e087a01d9f0d03335a083886155303b25dc15a
christian <christian.onyeneke@arkounting.com.ng>
2019-02-27 12:50:17 +0100

    added id to tasks table

--- database/migrations/2019_02_27_103230_tasks.php
+++ database/migrations/2019_02_27_103230_tasks.php
@@ -14,6 +14,7 @@ class Tasks extends Migration
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->string('title');
             $table->timestamps();
         });
